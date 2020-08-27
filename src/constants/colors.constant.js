5a62ec75897af7e26da62a8f5c0d79d56edf4262
Omar Ong <oong@wheelsup.com>
2020-08-27 17:53:08 -0400

    constant init, add styled-components

---
+++ src/constants/colors.constant.js
@@ -0,0 +1,17 @@
+export const colorConst = {
+    "primary": {
+        "rojo": "#ff0000",
+        "blanque": "#ffffff",
+        "azool": "#001aff",
+        "nadir": "#000000"
+    },
+    "zwei": {
+        "das-blau": "#042d58",
+        "fraulein-blau": "#4986cb"
+    },
+    "trois": {
+        "poopy":"#815e32",
+        "gnarly":"#4ebc39",
+        "slushy":"#ab9273"
+    }
+};
