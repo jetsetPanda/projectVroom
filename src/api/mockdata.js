a19d990fc05700bfaf7ddbca18543dc0b33ffd67
Omar Ong <oong@wheelsup.com>
2020-08-27 17:37:06 -0400

    added car imgs to mock data

---
+++ src/api/mockdata.js
@@ -0,0 +1,185 @@
+export const CarMakes = [
+    {
+        "id": "1dd25cb5-2b38-4f4f-ae25-81892f2136f7",
+        "make": "Chevrolet",
+        "model": "Camaro",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USD00CHC021A021001.jpg"
+    },
+    {
+        "id": "b8b875de-cc14-4023-a3a4-697ccce5fd1f",
+        "make": "Volvo",
+        "model": "S60",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC90VOC341A021001_2.jpg"
+    },
+    {
+        "id": "70df4e42-8d80-4e49-8901-1d551cfca146",
+        "make": "Infiniti",
+        "model": "JX35",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/CAC30INS111A021001.jpg"
+    },
+    {
+        "id": "824cba71-dc73-44ec-9000-e33792f251a6",
+        "make": "MINI",
+        "model": "Countryman",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USD10MNS031B01300.jpg"
+    },
+    {
+        "id": "90a62b83-7c87-484f-ac77-a0976b631fe7",
+        "make": "Mercury",
+        "model": "Sable",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USB90MEC041A0101.jpg"
+    },
+    {
+        "id": "0d36fe15-e4bd-4920-9896-bd668f7dc70f",
+        "make": "Oldsmobile",
+        "model": "Silhouette",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USB40OLV011B0101.jpg"
+    },
+    {
+        "id": "a2da2ed6-ddce-4b5a-b206-bf143994849a",
+        "make": "Nissan",
+        "model": "Quest",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC70NIV091B021001.jpg"
+    },
+    {
+        "id": "f146e70a-d050-420f-9900-de437704d9f1",
+        "make": "Kia",
+        "model": "Rio",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC90KIC031B021001.jpg"
+    },
+    {
+        "id": "60209592-b425-4530-9a28-a5f87caa3710",
+        "make": "Mazda",
+        "model": "323",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC90MAC171A021001.jpg"
+    },
+    {
+        "id": "576efa03-3707-4e72-840f-86587818eacd",
+        "make": "Maybach",
+        "model": "62",
+        "img": "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC20MYC021B0101.jpg"
+    }
+];
+
+
+export const inventory = [{
+    "id": 1,
+    "carId": "70df4e42-8d80-4e49-8901-1d551cfca146",
+    "quantity": 67,
+    "dateReceived": "1591924689"
+},
+    {
+        "id": 2,
+        "carId": "a2da2ed6-ddce-4b5a-b206-bf143994849a",
+        "quantity": 80,
+        "dateReceived": "1571478522"
+    },
+    {
+        "id": 3,
+        "carId": "1dd25cb5-2b38-4f4f-ae25-81892f2136f7",
+        "quantity": 52,
+        "dateReceived": "1580219537"
+    },
+    {
+        "id": 4,
+        "carId": "90a62b83-7c87-484f-ac77-a0976b631fe7",
+        "quantity": 3,
+        "dateReceived": "1592590366"
+    },
+    {
+        "id": 5,
+        "carId": "576efa03-3707-4e72-840f-86587818eacd",
+        "quantity": 45,
+        "dateReceived": "1570231767"
+    },
+    {
+        "id": 6,
+        "carId": "b8b875de-cc14-4023-a3a4-697ccce5fd1f",
+        "quantity": 55,
+        "dateReceived": "1591718298"
+    },
+    {
+        "id": 7,
+        "carId": "60209592-b425-4530-9a28-a5f87caa3710",
+        "quantity": 67,
+        "dateReceived": "1597705266"
+    },
+    {
+        "id": 8,
+        "carId": "1dd25cb5-2b38-4f4f-ae25-81892f2136f7",
+        "quantity": 32,
+        "dateReceived": "1575675316"
+    },
+    {
+        "id": 9,
+        "carId": "f146e70a-d050-420f-9900-de437704d9f1",
+        "quantity": 18,
+        "dateReceived": "1581723507"
+    },
+    {
+        "id": 10,
+        "carId": "a2da2ed6-ddce-4b5a-b206-bf143994849a",
+        "quantity": 94,
+        "dateReceived": "1590919622"
+    },
+    {
+        "id": 11,
+        "carId": "576efa03-3707-4e72-840f-86587818eacd",
+        "quantity": 5,
+        "dateReceived": "1579702403"
+    },
+    {
+        "id": 12,
+        "carId": "1dd25cb5-2b38-4f4f-ae25-81892f2136f7",
+        "quantity": 94,
+        "dateReceived": "1580483045"
+    },
+    {
+        "id": 13,
+        "carId": "f146e70a-d050-420f-9900-de437704d9f1",
+        "quantity": 42,
+        "dateReceived": "1594420121"
+    },
+    {
+        "id": 14,
+        "carId": "a2da2ed6-ddce-4b5a-b206-bf143994849a",
+        "quantity": 72,
+        "dateReceived": "1579958136"
+    },
+    {
+        "id": 15,
+        "carId": "90a62b83-7c87-484f-ac77-a0976b631fe7",
+        "quantity": 65,
+        "dateReceived": "1574693985"
+    },
+    {
+        "id": 16,
+        "carId": "576efa03-3707-4e72-840f-86587818eacd",
+        "quantity": 90,
+        "dateReceived": "1580289571"
+    },
+    {
+        "id": 17,
+        "carId": "90a62b83-7c87-484f-ac77-a0976b631fe7",
+        "quantity": 42,
+        "dateReceived": "1572545068"
+    },
+    {
+        "id": 18,
+        "carId": "576efa03-3707-4e72-840f-86587818eacd",
+        "quantity": 24,
+        "dateReceived": "1575834969"
+    },
+    {
+        "id": 19,
+        "carId": "70df4e42-8d80-4e49-8901-1d551cfca146",
+        "quantity": 63,
+        "dateReceived": "1566741900"
+    },
+    {
+        "id": 20,
+        "carId": "a2da2ed6-ddce-4b5a-b206-bf143994849a",
+        "quantity": 67,
+        "dateReceived": "1595832165"
+    }
+];
